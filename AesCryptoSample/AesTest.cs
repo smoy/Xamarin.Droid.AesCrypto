5640dd17acb1712983b8893c71a3d21416e1a0c5
Steven Moy <github@stevenmoy.com>
2016-06-15 08:55:09 -0700

    Initial Commit

---
+++ AesCryptoSample/AesTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+
+using Com.Tozny.Crypto.Android;
+using System.Text;
+
+namespace AesCryptoSample
+{
+  public class AesTest
+  {
+    public AesTest ()
+    {
+    }
+
+    [Fact]
+    public void TestRoundTrip ()
+    {
+      var privateKey = AesCbcWithIntegrity.GenerateKey ();
+      var mySecretText = "This is my secret";
+      var mySecretBytes = Encoding.UTF8.GetBytes (mySecretText);
+      var cipherText = AesCbcWithIntegrity.Encrypt (mySecretBytes, privateKey);
+      var decryptedBytes = AesCbcWithIntegrity.Decrypt (cipherText, privateKey);
+      var decryptedText = Encoding.UTF8.GetString (decryptedBytes);
+
+      System.Diagnostics.Contracts.Contract.Assert (mySecretText == decryptedText, 
+                                                    string.Format("Expect {0} but got {1}", mySecretText, decryptedText));
+    }
+  }
+}
+
---
+++ AesCryptoSample/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Xunit.Sdk;
+using Xunit.Runners.UI;
+using Android.App;
+using System.Reflection;
+
+namespace AesCryptoSample
+{
+  [Activity (Label = "xUnit Android Runner", MainLauncher = true, Theme = "@android:style/Theme.Material.Light")]
+  public class MainActivity : RunnerActivity
+  {
+
+    protected override void OnCreate (Bundle bundle)
+    {
+      // tests can be inside the main assembly
+      AddTestAssembly (Assembly.GetExecutingAssembly ());
+
+      AddExecutionAssembly (typeof (ExtensibilityPointFactory).Assembly);
+      // or in any reference assemblies     
+
+      //AddTestAssembly(typeof(PortableTests).Assembly);
+      // or in any assembly that you load (since JIT is available)
+
+      // you cannot add more assemblies once calling base
+      base.OnCreate (bundle);
+    }
+  }
+}
+
+
