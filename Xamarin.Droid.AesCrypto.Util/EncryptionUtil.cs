4a673b837c2aabf09b4fb630f5f118689f0ca388
Steven Moy <github@stevenmoy.com>
2016-06-15 20:42:51 -0700

    Add docstring

--- Xamarin.Droid.AesCrypto.Util/EncryptionUtil.cs
+++ Xamarin.Droid.AesCrypto.Util/EncryptionUtil.cs
@@ -5,12 +5,24 @@ using Com.Tozny.Crypto.Android;
 
 namespace Xamarin.Droid.AesCrypto.Util
 {
+  /// <summary>
+  /// EncryptionUtil exists because AndroidKeyStore does not play well with symmetric key (SecretKeyEntry)
+  /// until API 23. So we are doing a around about well to have a asymmetric key pair to wrap our
+  /// symmetric key; then we can store the encrypted symmetric key in SharedPreference (or whereever). Only
+  /// when you need to use the symmetric key, you use AndroidKeyStore to unwrap our enerypted symmetric key.
+  /// </summary>
   public class EncryptionUtil
   {
     public EncryptionUtil ()
     {
     }
 
+    /// <summary>
+    /// Encrypt the specified clearText.
+    /// </summary>
+    /// <param name="context">Your Android Context, likely your Activity or Service</param>
+    /// <param name="alias">Alias is the name you are using for the key, use sensible name</param>
+    /// <param name="clearText">The content you want to encrypt</param>
     public static EncryptedTuple Encrypt (Context context, string alias, string clearText)
     {
       var secretKeys = AesCbcWithIntegrity.GenerateKey ();
@@ -23,6 +35,12 @@ namespace Xamarin.Droid.AesCrypto.Util
       return new EncryptedTuple (encryptedBundle.ToString (), Convert.ToBase64String (wrappedConfidentialKey), Convert.ToBase64String (wrappedIntegrityKey));
     }
 
+    /// <summary>
+    /// Decrypt the encryptedTuple.
+    /// </summary>
+    /// <param name="context">Your Android Context, likely your Activity or Service</param>
+    /// <param name="alias">Alias is the name you are using for the key, use sensible name</param>
+    /// <param name="encryptedTuple">Encrypted tuple.</param>
     public static string Decrypt (Context context, string alias, EncryptedTuple encryptedTuple)
     {
       var confidentialKeyWrapper = new SecretKeyWrapper (context, alias + "-confidential");
